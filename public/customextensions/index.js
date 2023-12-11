ef0d91676dec7fcaeac66f868e433d0512c6e920
felipecarrillo100 <felipecarrillo100@gmail.com>
2023-12-12 00:54:26 +0100

    Adding painting settings

--- public/customextensions/index.js
+++ public/customextensions/index.js
@@ -776,7 +776,198 @@ const SampleCommand = {
         "selectable": true,
         "editable": false,
         "minScale": null,
-        "maxScale": null
+        "maxScale": null,
+        "painterSettings": {
+            "commons": {
+              "balloon": false,
+              "heatmap": {
+                "enabled": false,
+                "maximum": 20,
+                "normalizedGradient": [
+                  {
+                    "level": 0,
+                    "color": "rgba( 0 , 0 , 255, 0.9)"
+                  },
+                  {
+                    "level": 0.25,
+                    "color": "rgba( 0 , 255 , 255, 0.9)"
+                  },
+                  {
+                    "level": 0.5,
+                    "color": "rgba( 0 , 255 , 0, 0.9)"
+                  },
+                  {
+                    "level": 0.75,
+                    "color": "rgba( 255 , 255 , 0, 0.9)"
+                  },
+                  {
+                    "level": 1,
+                    "color": "rgba( 255 , 0 , 0, 0.9)"
+                  }
+                ]
+              }
+            },
+            "default": {
+              "labelStyle": {
+                "background": {
+                  "fill": {
+                    "color": "rgba(138, 200, 255, 0)"
+                  },
+                  "stroke": {
+                    "color": "rgba(138, 200, 255, 0)"
+                  }
+                },
+                "case": "none",
+                "font": {
+                  "bold": true,
+                  "fontFamily": "'Times New Roman', Times, serif",
+                  "fontSize": "14px",
+                  "italic": false,
+                  "underline": false
+                },
+                "labelProperty": "",
+                "stroke": {
+                  "color": "rgba(0,255,255,1)"
+                }
+              },
+              "lineStyle": {
+                "draped": true,
+                "drapeTarget": 1,
+                "stroke": {
+                  "color": "rgba(255,182,138,1)",
+                  "dashIndex": "solid",
+                  "width": 5
+                }
+              },
+              "pointStyle": {
+                "draped": true,
+                "drapeTarget": 1,
+                "fill": {
+                  "color": "rgba(138,200,255,0.8)"
+                },
+                "heading": {
+                  "default": "",
+                  "property": "",
+                  "scale": 1
+                },
+                "shape": "gradient-circle",
+                "size": {
+                  "height": 16,
+                  "width": 16
+                },
+                "anchorX": "",
+                "anchorY": "",
+                "stroke": {
+                  "color": "rgb(29,74,118)",
+                  "width": 1
+                },
+                "url": ""
+              },
+              "shapeStyle": {
+                "draped": true,
+                "drapeTarget": 1,
+                "fill": {
+                  "color": "rgba(255, 0, 0, 0.6)"
+                },
+                "stroke": {
+                  "color": "rgb(255, 255, 0)",
+                  "dashIndex": "solid",
+                  "width": 2
+                },
+                "extruded": {
+                  "type": "Flat",
+                  "height": {
+                    "property": "",
+                    "scale": 1
+                  },
+                  "minHeight": {
+                    "property": "",
+                    "scale": 1
+                  }
+                }
+              }
+            },
+            "painterType": "DefaultPainter",
+            "selected": {
+              "labelStyle": {
+                "background": {
+                  "fill": {
+                    "color": "rgba(138, 200, 255, 0.8)"
+                  },
+                  "stroke": {
+                    "color": "rgba(138, 200, 255, 0.8)"
+                  }
+                },
+                "case": "none",
+                "color": "rgba(0,0,255,1)",
+                "font": {
+                  "bold": true,
+                  "fontFamily": "'Times New Roman', Times, serif",
+                  "fontSize": "14px",
+                  "italic": false,
+                  "underline": false
+                },
+                "labelProperty": "",
+                "stroke": {
+                  "color": "rgba(0,255,255,1)"
+                }
+              },
+              "lineStyle": {
+                "draped": true,
+                "drapeTarget": 1,
+                "stroke": {
+                  "color": "rgb(0, 128, 256)",
+                  "dashIndex": "solid",
+                  "width": 2
+                }
+              },
+              "pointStyle": {
+                "draped": true,
+                "drapeTarget": 1,
+                "fill": {
+                  "color": "rgba(255, 200, 255, 0.8)"
+                },
+                "heading": {
+                  "default": "",
+                  "property": "",
+                  "scale": 1
+                },
+                "shape": "gradient-circle",
+                "size": {
+                  "height": 18,
+                  "width": 18
+                },
+                "anchorX": "",
+                "anchorY": "",
+                "stroke": {
+                  "color": "rgba(146,24,48,1)",
+                  "width": 1
+                },
+                "url": ""
+              },
+              "shapeStyle": {
+                "fill": {
+                  "color": "rgba(0, 255, 0, 0.6)"
+                },
+                "stroke": {
+                  "color": "rgb(255, 255, 255)",
+                  "dashIndex": "solid",
+                  "width": 2
+                },
+                "extruded": {
+                  "type": "Flat",
+                  "height": {
+                    "property": "",
+                    "scale": 1
+                  },
+                  "minHeight": {
+                    "property": "",
+                    "scale": 1
+                  }
+                }
+              }
+            }
+          },
       },
       "model": {
         "transformer": "TOMTOM ROUTE",
