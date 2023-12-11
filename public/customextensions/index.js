739ff41dad5db9150f3dba199f32a8a9c44c5005
felipecarrillo100 <felipecarrillo100@gmail.com>
2023-12-12 00:41:54 +0100

    Adding Tomtom API sample

--- public/customextensions/index.js
+++ public/customextensions/index.js
@@ -192,6 +192,22 @@ window.catex.featureLayer = {
                 }
             } 
         },
+        {
+            label: "Tomtom route",
+            title: "Calculate Route using Tomtom API",
+            action: function(o, callback) {
+                if (typeof callback === "function") {
+                    if (o.features) {
+                        if (o.features.length==2) {
+                            const point1 = o.features[0].shape.focusPoint;
+                            const point2 = o.features[1].shape.focusPoint;
+                            const newCommand = TomTomAPIURL([point1.y, point1.x], [point2.y, point2.x], 'Tomtom route');
+                            window.catex.workspace.emitCommand(newCommand);
+                        }
+                    }                    
+                }
+            } 
+        },
     ],
     onFeatureSelect: [
         {
@@ -342,6 +358,21 @@ window.catex.data = {
                     return {id, type:"Feature", geometry: geom, properties }
                 });
                 return JSON.stringify(geojsonArray);
+            }
+        },        
+        {
+            name: "TOMTOM ROUTE",
+            extensions: "json",
+            transform: (dataStr) => {
+                const tomtomResponse = JSON.parse(dataStr); 
+                const features = [];                
+                if (tomtomResponse.routes.length > 0 && tomtomResponse.routes[0].legs.length > 0) {                
+                    const coordinates = tomtomResponse.routes[0].legs[0].points.map(point=>[point.longitude, point.latitude]);
+                    const lineString = { type: 'LineString', coordinates};
+                    const feature = {id: 0, type:"Feature", geometry: lineString, properties:tomtomResponse.routes[0].summary};
+                    features.push(feature);
+                }
+                return JSON.stringify(features);
             }
         },
         {
@@ -724,6 +755,34 @@ const SampleCommand = {
         "beforeProxy": `http://localhost:5000/api/turf/${algorthm}`,
         method: "POST",
         body: JSON.stringify(featureCollection),
+        "format": "GeoJSON",
+        "crs": "CRS:84",
+        "swapAxes": false,
+        "credentials": false,
+        "requestHeaders": {
+            "Content-Type": "application/json"
+        }
+      }
+    }
+  })
+
+  const TomTomAPIURL = (point1, point2, label) => ({
+    "action": 10,
+    "parameters": {
+      "action": "MemoryFeatureLayer",
+      "autozoom": true,
+      "layer": {
+        "label": label,
+        "selectable": true,
+        "editable": false,
+        "minScale": null,
+        "maxScale": null
+      },
+      "model": {
+        "transformer": "TOMTOM ROUTE",
+        "url": `https://api.tomtom.com/routing/1/calculateRoute/${point1[0]},${point1[1]}:${point2[0]},${point2[1]}/json?&vehicleHeading=90&sectionType=traffic&report=effectiveSettings&routeType=eco&traffic=true&avoid=unpavedRoads&travelMode=car&vehicleMaxSpeed=120&vehicleCommercial=false&vehicleEngineType=combustion&key=A9R38rS8rPA6NS1ARfwn24mgEMlCCQ9k`,
+        "beforeProxy": `https://api.tomtom.com/routing/1/calculateRoute/${point1[0]},${point1[1]}:${point2[0]},${point2[1]}/json?&vehicleHeading=90&sectionType=traffic&report=effectiveSettings&routeType=eco&traffic=true&avoid=unpavedRoads&travelMode=car&vehicleMaxSpeed=120&vehicleCommercial=false&vehicleEngineType=combustion&key=A9R38rS8rPA6NS1ARfwn24mgEMlCCQ9k`,
+        method: "GET",
         "format": "GeoJSON",
         "crs": "CRS:84",
         "swapAxes": false,
